3459211a1b7efdf02ea488d31c7c6e6dea20cf1f
Shailika soni <shailika@Shailikas-MacBook-Air.local>
2025-12-31 01:50:23 +0530

    Feature: Persistent logging and refined volume mapping

--- app/utils/logging_utils.py
+++ app/utils/logging_utils.py
@@ -12,9 +12,19 @@ def setup_logger(name: str):
             '[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S'
         )
+        # Stream Handler
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # File Handler
+        log_dir = os.path.join(os.getcwd(), "logs")
+        os.makedirs(log_dir, exist_ok=True)
+        log_file = os.path.join(log_dir, "app.log")
         
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     
     return logger
